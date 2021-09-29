9bae82cd938e94f566c088b883148e0e3f10bcdb
capturetheworld <ian@iansoohoo.me>
2021-09-28 23:24:41 -0700

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const reactElement = <div>Heellllooooo</div>
+const domElement = document.getElementById('root')
+
+ReactDOM.render(reactElement, domElement)
